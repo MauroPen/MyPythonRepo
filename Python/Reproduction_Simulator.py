a5508f7390d2c94d4965883c9a398d2c25c9fd2f
MauroPen <mau.pennini@gmail.com>
2022-08-11 03:12:22 +0200

    Added multiple (fixed) iterations

--- Python/Reproduction_Simulator.py
+++ Python/Reproduction_Simulator.py
@@ -4,11 +4,13 @@ from numpy import random
 from tabulate import tabulate
 from matplotlib import pyplot
 
+#running = bool(True)
+
 print("Welcome to the Reproduction Simulator!\n")
 
 # Variables settings
 
-N = int(input("\nPlease input the Inital Number of Population that you want to set:\n", )) # Exceptions to be added
+Starting_N = int(input("\nPlease input the Inital Number of Population that you want to set:\n", )) # Exceptions to be added
 
 Period = int(input("\nAmazing! Now, please input the length of the Period of time that you want to consider for the simulation:\n")) # Exceptions to be added
 
@@ -22,38 +24,49 @@ c = float(input("\nFinally, please input the Crowding Coefficient. \nPAY ATTENTI
 
 Period_Tab = list(range(0, Period + 1, 1)) #Indexes the periods, necessary for plots
 
-N_Tab = [N] #Collects the resulting N each time
+pyplot.title("Simulation Results") 
+pyplot.xlabel("Period") 
+pyplot.ylabel("Population") 
 
-D_Tab =  [0]#Collects the resuling D each time
+for i in range(1, 31, 1): #Iterating the same simulation 30 times. Must become a parameter
 
-for i in range(1, Period + 1, 1):
+    N_Tab = [Starting_N] #Collects the resulting N each time for each iteration
 
-    D = 0 #Collects the Delta each time
+    D_Tab =  [0] #Collects the resuling D each time for each iteration
 
-    for j in range(1, N + 1, 1):
+    for j in range(1, Period + 1, 1):
 
-        br = random.binomial(1, b, 1)[0]
+        N = N_Tab[j - 1] #Sets the starting population for calculation
 
-        dr = random.binomial(1, d + (c * N), 1)[0]
+        D = 0 #Collects the Delta each time
 
-        if (br == 1):
+        for k in range(1, N + 1, 1):
 
-            D += 1
+            br = random.binomial(1, b, 1)[0]
 
-        elif (dr == 1):
+            dr = random.binomial(1, d + (c * N), 1)[0]
 
-            D -= 1
+            if (br == 1):
 
-        # print("\nProcessing! Please Wait...\n\nCurrent Person Evaluated: ", j, "/", N)
+                D += 1
 
-    D_Tab.append(D)
-    
-    N += D
+            elif (dr == 1):
 
-    N_Tab.append(N)
+                D -= 1
 
-# print("\nProcessing! Please Wait...\n\nCurrent Day: ", i, "/", Period)
+            #print("\nProcessing! Please Wait...\n\nCurrent Person Evaluated: ", k, "/", N)
 
+        N += D
+        
+        D_Tab.append(D) #Updates table
+
+        N_Tab.append(N) #Updates table
+
+    pyplot.plot(Period_Tab, N_Tab, linestyle = ":")
+
+    #print("\nProcessing! Please Wait...\n\nCurrent Day: ", j, "/", Period)
+
+"""
 # Results Presentation
 
 print("\n")
@@ -65,8 +78,5 @@ print("\nDeath Rate set: ", d)
 print("\nFinal_Population: ", N_Tab[Period])
 print("\nTotal Delta: ", sum(D_Tab))
 
-pyplot.title("Simulation Results") 
-pyplot.xlabel("Period") 
-pyplot.ylabel("Population") 
-pyplot.plot(Period_Tab, N_Tab)
+"""
 pyplot.show()
