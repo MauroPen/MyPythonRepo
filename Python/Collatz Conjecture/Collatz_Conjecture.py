91d837b2ea8f5c2040c71175773d779aac9b67f9
MauroPen <mau.pennini@gmail.com>
2022-09-18 22:24:22 +0200

    Designed the computation phase

--- Python/Collatz Conjecture/Collatz_Conjecture.py
+++ Python/Collatz Conjecture/Collatz_Conjecture.py
@@ -1,6 +1,8 @@
 # Collatz Conjecture
 
-from numpy import sort
+from numpy import sort, array, append
+from pandas import DataFrame, Series
+from IPython.display import display
 
 def yn_input_check():
 
@@ -50,7 +52,7 @@ def int_input_check():
 
 running = bool(True)
 
-Default_Range = [1, 50]
+Default_Range = array([10, 15])
 
 while (running == True):
 
@@ -60,9 +62,9 @@ while (running == True):
 
     Range = [1, 0] #Initialized to unsatisfy the condition in "else" 
 
-    Default_mode = yn_input_check()
+    Default_Mode = yn_input_check()
 
-    if (Default_mode == True):
+    if (Default_Mode == True):
 
         Range = Default_Range
 
@@ -86,7 +88,41 @@ while (running == True):
 
                     Range = sort(Range)
 
-    print("\n", Range, "\n")
+    # Computation
+
+    Execution_Table = DataFrame({"Starting Numbers": list(range(Range[0], Range[1] + 1, 1))}, index = list(range(1, (Range[1] - Range[0] + 2), 1))) #Creating the table collecting valuesobtained for each starting number
+
+    Execution_Table["Obtained Values"] = Series(dtype = object) #Creating an empty column hosting the obtained values in array form
+
+    Execution_Table["Obtained Values"] = Execution_Table["Obtained Values"].apply(lambda column: [])
+
+    Max_Number = Range[1]
+
+    Max_Iterations = 0
+
+    for i in range(1, (Range[1] - Range[0] + 2), 1):
+
+        Iteration_Array = array([Range[0] + i - 1])
+
+        while(Iteration_Array[-1] != 1):
+
+            if (Iteration_Array[-1] % 2 == 0):
+
+                Iteration_Array = append(Iteration_Array, Iteration_Array[-1] / 2)
+
+            else:
+
+                Iteration_Array = append(Iteration_Array, Iteration_Array[-1] * 3 + 1)
+
+        if (Max_Number < max(Iteration_Array)):
+
+            Max_Number = max(Iteration_Array)
+
+        if (Max_Iterations < (len(Iteration_Array) - 1)):
+
+            Max_Iterations = (len(Iteration_Array) - 1)
+
+        Execution_Table.at[i, "Obtained Values"] = Iteration_Array.astype(int)
 
     print("\nComputation ended!\n\nDo you want to start over? (y/n)\n")
     
