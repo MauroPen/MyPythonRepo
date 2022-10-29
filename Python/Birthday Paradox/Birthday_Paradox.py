98b26980b797050ec5e78e45e1be4a8192a28e04
MauroPen <mau.pennini@gmail.com>
2022-10-30 00:21:17 +0200

    class Person created, peopleTable intialized

--- Python/Birthday Paradox/Birthday_Paradox.py
+++ Python/Birthday Paradox/Birthday_Paradox.py
@@ -1,4 +1,15 @@
+from numpy import append
+from pandas import DataFrame
+from datetime import date
+from random import randint
 
+
+class Person:
+
+    def __init__(self, Id, Birthday):
+
+        self.id = Id
+        self.birthday = Birthday
 
 def yn_input_check():
 
@@ -44,6 +55,10 @@ def int_input_check():
 
             int_input = -1
 
+def generate_random_birthday(startDate, endDate):
+
+    return date.fromordinal(randint(startDate, endDate))
+
 #Setting Default Values
 
 running = bool(True)
@@ -52,6 +67,9 @@ defaultValues = {
     "People": 23,
     "Trials": 1000
 }
+
+startDate_Birthdays = date.today().replace(day = 1, month = 1, year = 1922).toordinal()   #Setting the lowest possible birthday admitted
+endDate_Birthdays = date.today().replace(day = 31, month = 12, year = 2021).toordinal()   #Setting the highest possible birthday admitted
 
 while (running):
 
@@ -74,4 +92,26 @@ while (running):
             
         values["Trials"] = int_input_check()
 
-    print(values)
+    #Computation
+
+    trialsList = list(range(1, values["Trials"] + 1, 1))
+
+    peopleList = list(range(1, values["People"] + 1, 1))
+
+    peopleTable = DataFrame(columns = ["Trial", "Person_Id", "Birthday"]) #Creating the table collecting data about all the people generated
+
+    for trial in trialsList:
+
+        people = []                #Creating an array of people for each trial
+
+        for personId in peopleList:
+    
+            people = append(people, Person(personId, generate_random_birthday(startDate_Birthdays, endDate_Birthdays)))
+
+            peopleTable.at[personId, "Trial"] = trial
+
+            peopleTable.at[personId, "Person_Id"] = people[personId - 1].id
+
+            peopleTable.at[personId, "Birthday"] = people[personId - 1].birthday
+
+            print("\n", people[personId - 1].id, people[personId - 1].birthday)
