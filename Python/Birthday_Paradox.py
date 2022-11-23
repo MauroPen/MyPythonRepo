06c857ee7ed1a4bab514e1903727f2405db1461c
MauroPen <mau.pennini@gmail.com>
2022-11-23 01:12:04 +0100

    move functions to Common.py

--- Python/Birthday_Paradox.py
+++ Python/Birthday_Paradox.py
@@ -6,6 +6,8 @@ from math import factorial
 from tabulate import tabulate
 from os import getcwd
 
+from Common import yn_input_check, int_input_check
+
 
 class Person:
 
@@ -15,49 +17,6 @@ class Person:
         self.birthday = Birthday
         self.birthday_match = False         #False by default, checked later
 
-def yn_input_check():
-
-    char_input = ""
-
-    while(not(char_input == "y" or char_input == "Y" or char_input == "n" or char_input == "N")):
-
-        char_input = input()
-
-        if (char_input == "y" or char_input == "Y"):
-
-            return True
-
-        elif (char_input == "n" or char_input == "N"):
-
-            return False
-
-        else:
-
-            print("\nThe inserted value is not valid, please input \"y\" or \"n\"\n")
-
-def int_input_check():
-
-    int_input = -1
-
-    while (int_input < 0):
-
-        try:
-            
-            int_input = int(input())
-    
-            while(int_input < 0):
-
-                print("\nThe inserted value is not valid, please input a number higher than 0:\n")
-
-                int_input = int(input())
-
-            return int_input
-        
-        except:
-
-            print("\nThe inserted value is not valid, please input an integer number higher than 0:\n")
-
-            int_input = -1
 
 def generate_random_birthday(startDate, endDate):
 
--- Python/Common.py
+++ Python/Common.py
@@ -1,5 +1,5 @@
 
-
+# 1 - yn_input_check: asks for a y/n input (case insensitive) by the user. Anything else is not accepted.
 def yn_input_check():
 
     char_input = ""
@@ -19,3 +19,28 @@ def yn_input_check():
         else:
 
             print("\nThe inserted value is not valid, please input \"y\" or \"n\"\n")
+
+#2 - int_input_check: asks for an integer by the user, then returns that value. Anything else is not accepted.
+def int_input_check():
+
+    int_input = -1
+
+    while (int_input < 0):
+
+        try:
+            
+            int_input = int(input())
+    
+            while(int_input < 0):
+
+                print("\nThe inserted value is not valid, please input a number higher than 0:\n")
+
+                int_input = int(input())
+
+            return int_input
+        
+        except:
+
+            print("\nThe inserted value is not valid, please input an integer number higher than 0:\n")
+
+            int_input = -1
