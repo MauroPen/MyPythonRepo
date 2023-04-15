123f360e98ada1dc1798f466af9fd16749f4d28f
MauroPen <mau.pennini@gmail.com>
2023-04-15 19:46:34 +0200

    add possibility to save pi_until in a .txt file

--- Scripts/Check_Numbers_Pi.py
+++ Scripts/Check_Numbers_Pi.py
@@ -1,4 +1,5 @@
-import math_pi
+import re
+import os
 
 from Common import yn_input_check
 from Check_Numbers_Pi.Dependency import number_check, main_computation, check_occurrencies
@@ -19,7 +20,7 @@ running = True
 while(running):
 
     # Take user input
-    print("\nWelcome! Please, input the set of consecutive digits to be found inside pi: ", end = "")
+    print("\nWelcome! Please, input the set of consecutive digits to be found inside pi:\n")
     number = number_check()
 
     # Running computation
@@ -31,15 +32,27 @@ while(running):
 
     else:
 
-        print("\n{digits} pi digits have been checked to find {input}.\nHereby, all pi digits until the first occurrence of {input} are printed:\n".format(digits = result["Digits Checked"], input = number))
-        print(result["Pi Until"][:-len(number)], end = "")
+        print("\n{digits} pi digits have been checked before finding {input}.\nHereby, a snapshot of where to find its first occurrence is provided:\n".format(digits = result["Digits Checked"], input = number))
+        print(re.sub(r"^(.{500}).*(.{500})$", "\g<1>...\g<2>", result["Pi Until"][:-len(number)]), end = "")
         print(bcolors.OKGREEN + bcolors.BOLD + str(number) + bcolors.ENDC)
 
-        print("\nWould you like to check how many times the combination is present within pi's first million of digits? y/n")
+        if (len(result["Pi Until"][:-len(number)]) >= 1000):
+
+            print("\nWould you like to save the whole value of pi until the first occurrence of {combination} in a .txt file in your current directory? y/n".format(combination = number))
+            print("WARNING! A new .txt file will be create in your current working directory, which is: {current_working_directory}\n" .format(current_working_directory = os.getcwd()))
+
+            if (yn_input_check()):
+
+                with open("Pi_Until_{combination}.txt".format(combination = number), "w") as file:
+                    
+                    file.write(result["Pi Until"])
+        
+        print("\nWould you like to check how many times the combination is present within pi's first million of digits? y/n\n")
         
         if (yn_input_check()):
 
-            print("\nThe combination {Combination} has been found {Occurrencies} times.".format(Combination = number, Occurrencies = check_occurrencies(number, result["Digits Checked"])))
+            print("\nThe combination {combination} has been found {occurrencies} times.".format(combination = number, occurrencies = check_occurrencies(number, result["Digits Checked"])))
 
-    print("\nWould you like to start over? y/n")
+    print("\nWould you like to start over? y/n\n")
+
     running = yn_input_check()
