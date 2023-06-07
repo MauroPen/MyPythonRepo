7e303ca1464a45e176488e96fbb772b64f2d9b75
MauroPen <mau.pennini@gmail.com>
2023-06-08 00:25:51 +0200

    create schedule table

--- Scripts/f1_calendar_tmp.py
+++ Scripts/f1_calendar_tmp.py
@@ -1,4 +1,4 @@
-from pandas import DataFrame
+from pandas import DataFrame, concat
 from numpy import append
 
 from Common import DataframeExport, export_dataframes 
@@ -7,20 +7,24 @@ import fastf1
 
 fastf1.Cache.enable_cache("C:/Users/maupe/Desktop/FastF1_Cache")
 
-dataframes = []
+schema = DataFrame(columns = list(fastf1.get_event_schedule(1950, include_testing = False).columns))
 
-for i in range(2010, 2024, 1):
+for year in range(1950, 2024, 1):
+
+    print(" Retrieving calendar schedule from {Year}" .format(Year = year), end = '\r')
 
     try:
 
-        calendar = fastf1.get_event_schedule(i)
+        calendar = fastf1.get_event_schedule(year, include_testing = False)    # Excluding testing session to allow easy referencing
 
-        calendarExport = DataframeExport(calendar, "{number}" .format(number = i), False)
-
-        dataframes = append(dataframes, calendarExport)
+        schema = concat([schema, calendar], ignore_index = True)
 
     except:
 
-        print("An error occured!\n")
+        print("\n\nAn error occured!\n")
 
-export_dataframes(dataframes, "Schedule")
+schema.insert(0, "ChampionshipYear", schema["EventDate"].dt.year)
+
+calendarExport = DataframeExport(schema, "Schedule", False)
+
+export_dataframes([calendarExport], "Schedule")
